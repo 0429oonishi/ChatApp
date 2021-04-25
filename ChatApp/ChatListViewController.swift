8b57348484d9d1ad4d7945fe7f0dc52405c3c7ad
0429oonishi <oonishireon110@gmail.com>
2021-04-25 23:17:53 +0900

    ChatListVCのレイアウト作成

--- ChatApp/ChatListViewController.swift
+++ ChatApp/ChatListViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 
-class ChatListViewController: UIViewController {
+final class ChatListViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+// MARK: - UITableViewDelegate
+extension ChatListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - UITableViewDataSource
+extension ChatListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ChatListTableViewCell.identifier, for: indexPath) as! ChatListTableViewCell
+        return cell
+    }
+    
+}
 
+final class ChatListTableViewCell: UITableViewCell {
+    
+    static var identifier: String { String(describing: self) }
+    
+    @IBOutlet private weak var userImageView: UIImageView!
+    @IBOutlet private weak var latestMessageLabel: UILabel!
+    @IBOutlet private weak var partnerLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = userImageView.frame.size.width / 2
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: true)
+        
+    }
+    
 }
