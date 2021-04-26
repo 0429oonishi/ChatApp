4b291feb2db53393f6a4a3a2d78d16994db282ed
0429oonishi <oonishireon110@gmail.com>
2021-04-26 19:11:46 +0900

    Firestoreに保存する処理の実装

--- ChatApp/Controllers/SignUpViewController.swift
+++ ChatApp/Controllers/SignUpViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 final class SignUpViewController: UIViewController {
     
@@ -18,6 +21,15 @@ final class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.delegate = self
+        
+        profileImageButton.addTarget(self, action: #selector(profileImageButtonDidTapped), for: .touchUpInside)
+        registerButton.isEnabled = false
+        registerButton.backgroundColor = .rgb(red: 100, green: 100, blue: 100)
+        registerButton.addTarget(self, action: #selector(registerButtonDidTapped), for: .touchUpInside)
                 
     }
     
@@ -40,5 +52,88 @@ private extension SignUpViewController {
         profileImageButton.layer.borderColor = UIColor.rgb(red: 240, green: 240, blue: 240).cgColor
         registerButton.layer.cornerRadius = 10
     }
+    
+}
+
+// MARK: - @objc func
+@objc private extension SignUpViewController {
+    
+    func profileImageButtonDidTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func registerButtonDidTapped() {
+        guard let email = emailTextField.text,
+              let password = passwordTextField.text else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("情報の保存に失敗しました\(error)")
+                return
+            }
+            print("情報の保存に成功しました")
+            guard let uid = result?.user.uid else { return }
+            guard let username = self.usernameTextField.text else { return }
+            let docData = [
+                "email": email,
+                "username": username,
+                "createdAt": Timestamp(),
+            ] as [String: Any]
+            Firestore.firestore().collection("users").document(uid).setData(docData) { error in
+                if let error = error {
+                    print("Firestoreの保存に失敗しました\(error)")
+                    return
+                }
+                print("Firestoreの保存に成功しました。")
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+        }
+    }
+    
+}
+
+// MARK: - UITextFieldDelegate
+extension SignUpViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let emailIsEmpty = emailTextField.text?.isEmpty,
+              let passwordIsEmpty = passwordTextField.text?.isEmpty,
+              let usernameIsEmpty = usernameTextField.text?.isEmpty else { return }
+        if emailIsEmpty || passwordIsEmpty || usernameIsEmpty {
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = .rgb(red: 100, green: 100, blue: 100)
+        } else {
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = .systemOrange
+        }
+    }
+    
+}
+
+
+// MARK: - UIImagePickerControllerDelegate
+extension SignUpViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage {
+            profileImageButton.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            profileImageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        profileImageButton.setTitle("", for: .normal)
+        profileImageButton.imageView?.contentMode = .scaleAspectFill
+        profileImageButton.contentHorizontalAlignment = .fill
+        profileImageButton.contentVerticalAlignment = .fill
+        profileImageButton.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+// MARK: - UINavigationControllerDelegate
+extension SignUpViewController: UINavigationControllerDelegate {
     
 }
