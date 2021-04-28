6dcc2e45d0a8313b5a5fe9be309c9e348b048a4d
0429oonishi <oonishireon110@gmail.com>
2021-04-28 19:12:53 +0900

    Entities, Repositorys, Usecasesフォルダを作成

---
+++ ChatApp/Models/Repositorys/UserRepository.swift
@@ -0,0 +1,16 @@
+//
+//  UserRepository.swift
+//  ChatApp
+//
+//  Created by 大西玲音 on 2021/04/28.
+//
+
+import UIKit
+
+protocol UserRepository: class {
+   
+}
+
+class UserRepositoryImpl: UserRepository {
+    
+}
---
+++ ChatApp/Models/Usecases/UserUsecase.swift
@@ -0,0 +1,20 @@
+//
+//  UserUsecase.swift
+//  ChatApp
+//
+//  Created by 大西玲音 on 2021/04/28.
+//
+
+import Foundation
+
+class UserUsecase {
+    
+    let repository: UserRepository
+    
+    init(repository: UserRepository) {
+        self.repository = repository
+    }
+    
+    
+    
+}
